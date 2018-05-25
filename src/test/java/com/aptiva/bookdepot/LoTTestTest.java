de5f8dc279deafabb86f61c1b3264e132dbe6562
janardhanbonu <janardhanbonu@gmail.com>
2018-05-25 06:12:23 +0530

    unit test cases

---
+++ src/test/java/com/aptiva/bookdepot/LoTTestTest.java
@@ -0,0 +1,24 @@
+package com.aptiva.bookdepot;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoTTestTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test1() throws  Exception {
+        assert(true);
+    }
+}
